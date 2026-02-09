fb6a598fd9f4a0344141f63b3ce55b7ea4f3dc2a
peemisus <peem1433@gmail.com>
2026-02-09 21:32:48 +0700

    5.9 count

--- cell05/ex09/scan_it.py
+++ cell05/ex09/scan_it.py
@@ -2,7 +2,7 @@
 import sys
 if (len(sys.argv)==3):
  word_search = sys.argv[1]
- count = sys.argv[2].lower().count(word_search)
+ count = sys.argv[2].lower().count(word_search.lower())
  print(count)
 else:
  print("none")
