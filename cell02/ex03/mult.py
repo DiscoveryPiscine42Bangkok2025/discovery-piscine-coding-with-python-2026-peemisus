7abe87307516c48951e6b42001515e03f941c9dd
peemisus <peem1433@gmail.com>
2026-02-09 16:28:39 +0700

    2.3 mult

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+first_num = int(input("Enter the first number:\n").replace(" ",""))
+second_num =int(input("Enter the second number:\n").replace(" ",""))
+result = first_num*second_num
+print (f"{first_num} x {second_num} = {result}")
+if result>0 :
+ print("The result is positive")
+elif result<0:
+ print("The result is negative.")
+else:
+ print("The result is positive and negative.")
+
+
